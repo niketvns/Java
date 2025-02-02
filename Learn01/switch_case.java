459cf84a509ed8473b94978e583468c6f5802963
NIKET KUMAR MISHRA <niketvns97@gmail.com>
2025-02-02 12:40:31 +0530

    switch-case

---
+++ Learn01/switch_case.java
@@ -0,0 +1,45 @@
+public class switch_case {
+    public static void main(String[] args) {
+        int n = 1;
+        // ? Task: Print name of the day in week
+        switch (n) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+            default:
+                System.out.println("No Day Found!");
+        }
+
+        // ? New Switch case in java v14+
+        switch (n) {
+            case 0, 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            // throw new IllegalArgumentException("Invalid day")
+            default -> System.out.println("No Day Found!");
+        }
+    }
+}
