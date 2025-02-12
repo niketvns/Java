f4893dbf9d7de7f428fbce95ff1ae511a84c6759
NIKET KUMAR MISHRA <niketvns97@gmail.com>
2025-02-12 22:33:02 +0530

    Constructors in Java

--- Learn01/Encapsulation.java
+++ Learn01/Encapsulation.java
@@ -2,6 +2,25 @@ class Human {
     private int age;
     private String name;
 
+    // Constructors
+
+    // Default Constructor
+    public Human() {
+        age = 12;
+        name = "John";
+    }
+
+    // Parameterized Constructor
+    public Human(int age) {
+        this.age = age;
+    }
+
+    // Parameterized Constructor
+    public Human(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
     public void setAge(int a) {
         age = a;
     }
@@ -25,6 +44,11 @@ public class Encapsulation {
         h1.setAge(24);
         h1.setName("Niket Mishra");
 
+        Human h2 = new Human(19);
+        Human h3 = new Human(23, "Niket");
+
         System.out.println("Hello " + h1.getName() + " Your age is: " + h1.getAge());
+        System.out.println("Hello " + h2.getName() + " Your age is: " + h2.getAge());
+        System.out.println("Hello " + h3.getName() + " Your age is: " + h3.getAge());
     }
 }
